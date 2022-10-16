2867630cdf7861d044670421111161be84f5c696
gato365 <immanuelwilliams@gato365.com>
2022-10-16 06:40:07 -0700

    Add comments for plans for JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+// Use 
+// Use Activity 10, 12 from Web APIs to make timers
+// Use Activity 18 from Web APIs to go through questions
+// USe Activity 20 from Web APIs to click on multiple choice questions
+// Use Activity 22 from Web APIs to make high score list
+// Use Mini Project from Web APIs to do the following
+//      a) Start button
+//      b) Reduce time
+//      c) Keep all information on one page [CP,Q,HS,LP]
+//      d) ...
