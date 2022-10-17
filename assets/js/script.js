6cd658aa7d8f0ac6dea3501dfd47d310ae62bc84
gato365 <immanuelwilliams@gato365.com>
2022-10-17 04:42:35 -0700

    All of has been donegit add .git add .!

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,49 @@
 //      b) Reduce time
 //      c) Keep all information on one page [CP,Q,HS,LP]
 //      d) ...
+// ['Commonly used data types DO Not Include:',
+// 'The condition in an if / else statement is enclosed with ______.',
+// 'Arrays in JavaScript can be used to store_______.'
+// ]
+
+// [['1. string','2. booleans','3. alerts','4. numbers'],
+// ['1. quotes','2. curly brackets','3. parenthesis','4. square brackets'],
+// ['1. numbers and strings','2. other arrays','3. booleans','4. all of the above']
+// ]
+
+// 1. Address Button
+var startQuizButton = document.getElementById("start-quiz");
+// startQuizButton.addEventListner
+startQuizButton.onclick = startQuiz;
+
+var timeLeftEL = document.querySelector('.timer-count');
+var startDescriptionElement = document.querySelector('.first-screen-descrip');
+var timerId; 
+var timeLeft = 120;
+
+function startQuiz(){
+    console.log('Start Quiz');
+    startDescriptionElement.setAttribute('class','hide-me');
+    // Start Timer
+    timerId = setInterval(timerFunction,1000);
+    timeLeftEL.innerHTML = 'time-left: ' + timeLeft;
+}
 
 
-['Commonly used data types DO Not Include:',
-'The condition in an if / else statement is enclosed with ______.',
-'Arrays in JavaScript can be used to store_______.'
-]
+function timerFunction(){
+    timeLeft--;
+    timeLeftEL.innerHTML = 'time-left: ' + timeLeft;
+}
 
-[['1. string','2. booleans','3. alerts','4. numbers'],
-['1. quotes','2. curly brackets','3. parenthesis','4. square brackets'],
-['1. numbers and strings','2. other arrays','3. booleans','4. all of the above']
-]
+
+
+
+
+
+
+
+
+
+
+
+
