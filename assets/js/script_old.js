e6e9b22a5abdc71829affc93d4a0b1c6cc9c7574
gato365 <immanuelwilliams@gato365.com>
2022-10-20 02:27:24 -0700

    Change javaScript back to fix what i already i have

--- assets/js/script_old.js
+++ assets/js/script_old.js
@@ -1,84 +1,11 @@
+// Create Questions that will be asked
 
 
 
-// Use 
-// Use Activity 10, 12 from Web APIs to make timers
-// Use Activity 18 from Web APIs to go through questions
-// USe Activity 20 from Web APIs to click on multiple choice questions
-// Use Activity 22 from Web APIs to make high score list
-// Use Mini Project from Web APIs to do the following
-//      a) Start button
-//      b) Reduce time
-//      c) Keep all information on one page [CP,Q,HS,LP]
-//      d) ...
-var questionsList = [ 
-    {
-        'question': 'Commonly used data types DO Not Include:',
-        'choices':['string','booleans','alerts','numbers'],
-        'rightAnswer':  'alerts'
-    },
-
-   {
-    'question': 'The condition in an if / else statement is enclosed with ______.',
-    'choices':  ['quotes','curly brackets','parenthesis','square brackets'],
-    'rightAnswer': 'curly brackets'  
-   },
-   {
-    'question': 'Arrays in JavaScript can be used to store_______.',
-    'choices':  ['numbers and strings','other arrays','booleans','all of the above'],
-    'rightAnswer': 'all of the above'  
-   },
-   {
-    'question': 'String values must be enclosed within _____ when being assigned to variables',
-    'choices':  ['commas','curly bracket','quotes','parenthesis'],
-    'rightAnswer': 'quotes'  
-   },
-   {
-    'question': 'A very useful tool used during development and debugging for printing content to the debugger is:',
-    'choices':  ['JavaScript','terminal/bash','for loops','console.log'],
-    'rightAnswer': 'console.log'  
-   },
-
- 
-]
-
-
-
-// 1. Address Button
 var startQuizButton = document.getElementById("start-quiz");
 startQuizButton.onclick = startQuiz;
 
 var timeLeftEL = document.querySelector('.timer-count');
-var startDescriptionElement = document.querySelector('.first-screen-descrip');
-var entireQuestionElement = document.querySelector('.entire-question');
-var stemOfQuestionElement = document.querySelector('#stem-of-question');
-var choice1Element = document.querySelector('#choice1');
-var choice2Element = document.querySelector('#choice2');
-var choice3Element = document.querySelector('#choice3');
-var choice4Element = document.querySelector('#choice4');
-
-
-// Get Click by Usert
-// SHould be a function
-var choice1Button = document.getElementById('choice1');
-choice1Button.onclick = choice1selected;
-
-var choice2Button = document.getElementById('choice2');
-choice2Button.onclick = choice2selected;
-
-var choice3Button = document.getElementById('choice3');
-choice3Button.onclick = choice3selected;
-
-var choice4Button = document.getElementById('choice4');
-choice4Button.onclick = choice4selected;
-
-
-
-
-var timerId; 
-var timeLeft = 120;
-var currentQuestion;
-var questionIndex = 0;
 
 function startQuiz(){
     console.log('Start Quiz');
@@ -97,89 +24,165 @@ function timerFunction(){
     timeLeftEL.innerHTML = 'time-left: ' + timeLeft;
 }
 
+const Questions = [{
+		questionId: 0,
+		questionStem: 'Commonly used data types DO Not Include:',
+		questionSolution: [{ text: 'string', isCorrect: false },
+			{ text: 'booleans', isCorrect: false },
+			{ text: 'alerts', isCorrect: true },
+			{ text: 'numbers', isCorrect: false }
+		]
 
-function getNextQuestion(){
-    console.log('Get Next Question');
-    currentQuestion = questionsList[questionIndex];
-    stemOfQuestionElement.innerHTML = currentQuestion.question;
-    choice1Element.innerHTML = currentQuestion.choices[0];
-    choice2Element.innerHTML = currentQuestion.choices[1];
-    choice3Element.innerHTML = currentQuestion.choices[2];
-    choice4Element.innerHTML = currentQuestion.choices[3];
-    // Issue: Have to make options random
+	},
+	{
+		questionId: 1,
+		questionStem: 'The condition in an if / else statement is enclosed with ______.',
+		questionSolution: [{ text: 'square brackets', isCorrect: false },
+			{ text: 'parenthesis', isCorrect: false },
+			{ text: 'quotes', isCorrect: false },
+			{ text: 'curly brackets', isCorrect: true }
+		]
 
-    // Tasks:
-    // What did the user select? () --> use console.log to print it first
-    // Check the user's choice with correct solution. --> Use if statement
-    // If wrong, say wrong and add time to clock
-    // If right, say correct and proceed to next question
-    // Create While loop based on the following conditions - all questions are done or time runs out
-    // Hide Page and show enter Last page
-    // Top score list** 
+	},
+	{
+		questionId: 2,
+		questionStem: 'Arrays in JavaScript can be used to store_______.',
+		questionSolution: [{ text: 'booleans', isCorrect: false },
+			{ text: 'other arrays', isCorrect: false },
+			{ text: 'all of these options', isCorrect: true },
+			{ text: 'numbers and strings', isCorrect: false }
+		]
+
+	},
+
+	{
+		questionId: 3,
+		questionStem: 'String values must be enclosed within _____ when being assigned to variables',
+		questionSolution: [{ text: 'commas', isCorrect: false },
+			{ text: 'curly bracket', isCorrect: false },
+			{ text: 'quotes', isCorrect: true },
+			{ text: 'parenthesis', isCorrect: false }
+		]
+
+	},
+
+	{
+		questionId: 4,
+		questionStem: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+		questionSolution: [{ text: 'JavaScript', isCorrect: false },
+			{ text: 'for loops', isCorrect: false },
+			{ text: 'console.log', isCorrect: true },
+			{ text: 'terminal/bash', isCorrect: false }
+		]
+
+	}
 
 
-    
-    
+]
 
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+	// Getting the result display section
+	var result = document.getElementsByClassName("entire-question");
+	result[0].innerText = "";
+
+	// Getting the question
+	const question = document.getElementById("stem-of-question");
+
+
+	// Setting the question text
+	question.innerText = Questions[questionId].questionStem;
+
+	// Getting the options
+	const choice1 = document.getElementById('choice1');
+	const choice2 = document.getElementById('choice2');
+	const choice3 = document.getElementById('choice3');
+	const choice4 = document.getElementById('choice4');
+
+
+	// Providing option text
+	choice1.innerText = Questions[id].questionSolution[0].text;
+	choice2.innerText = Questions[id].questionSolution[1].text;
+	choice3.innerText = Questions[id].questionSolution[2].text;
+	choice4.innerText = Questions[id].questionSolution[3].text;
+
+	// Providing the true or false value to the options
+	choice1.value = Questions[questionId].questionSolution[0].isCorrect;
+	choice2.value = Questions[questionId].questionSolution[1].isCorrect;
+	choice3.value = Questions[questionId].questionSolution[2].isCorrect;
+	choice4.value = Questions[questionId].questionSolution[3].isCorrect;
+
+	var selected = "";
+
+	// Show selection for choice1
+	choice1.addEventListener("click", () => {
+		choice1.style.backgroundColor = "lightgoldenrodyellow";
+		choice2.style.backgroundColor = "lightskyblue";
+		choice3.style.backgroundColor = "lightskyblue";
+		choice4.style.backgroundColor = "lightskyblue";
+		selected = choice1.value;
+	})
+
+	// Show selection for choice2
+	choice2.addEventListener("click", () => {
+		choice1.style.backgroundColor = "lightskyblue";
+		choice2.style.backgroundColor = "lightgoldenrodyellow";
+		choice3.style.backgroundColor = "lightskyblue";
+		choice4.style.backgroundColor = "lightskyblue";
+		selected = choice2.value;
+	})
+
+	// Show selection for choice3
+	choice3.addEventListener("click", () => {
+		choice1.style.backgroundColor = "lightskyblue";
+		choice2.style.backgroundColor = "lightskyblue";
+		choice3.style.backgroundColor = "lightgoldenrodyellow";
+		choice4.style.backgroundColor = "lightskyblue";
+		selected = choice3.value;
+	})
+
+	// Show selection for choice4
+	choice4.addEventListener("click", () => {
+		choice1.style.backgroundColor = "lightskyblue";
+		choice2.style.backgroundColor = "lightskyblue";
+		choice3.style.backgroundColor = "lightskyblue";
+		choice4.style.backgroundColor = "lightgoldenrodyellow";
+		selected = choice4.value;
+	})
+
+	// Grabbing the evaluate button
+	const evaluate = document.getElementsByClassName("evaluate");
+
+	// Evaluate method
+	evaluate[0].addEventListener("click", () => {
+		if (selected == "true") {
+			result[0].innerHTML = "True";
+			result[0].style.color = "green";
+		} else {
+			result[0].innerHTML = "False";
+			result[0].style.color = "red";
+		}
+	})
 }
 
-function choice1selected(){
-    console.log('Choice - 1');
+if (start) {
+	iterate("0");
 }
 
-function choice2selected(){
-    console.log('Choice - 2');
-}
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var questionId = 0;
 
-function choice3selected(){
-    console.log('Choice - 3');
-}
+next.addEventListener("click", () => {
+	start = false;
+	if (questionId < 2) {
+		questionId++;
+		iterate(questionId);
+		console.log(questionId);
+	}
 
-function choice4selected(){
-    console.log('Choice - 4');
-}
-
-
-
-// // Check Answer
-// function checkSelection(){
-//     console.log('Check answer Answer');
-// }
-
-// // Wrong Answe Function
-// function wrongAnswer(){
-//     console.log('Wrong Answer');
-// }
-
-
-// // Corrrect Answer function
-// function correctAnswer(){
-//     console.log('Correct Answer');
-// }
-
-
-
-// Author: 
-// Date Created:
-// Date Modified: 
-// Name: 
-// Purpose:
-// Input:
-// Output:
-// Notes:
-
-
-
-
-
-//  (will not use)Random Set of Numbers not in order
-let array = [1, 2, 3, 4, 5, 6];
-const randomValue = array[Math.floor(Math.random() * array.length)];
-console.log(randomValue)
-
-
-
-
-
-
-
+})
